641ef8bce49ddc3b85ca419267ad4f436f35e5cb
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-06-27 09:06:04 +0100

    feat:updated password reset UI

--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -10,12 +10,8 @@ const mail = {
 const sendVerificationEmail = async ({ name, email, token, origin }) => {
   console.log(origin);
   const verificationLink = `${origin}/user/verify-email?token=${token}&email=${email}`;
-  const message = `<p>${token}</p><p>Please verify your email by clicking on the following link : 
-  <a href="${verificationLink}">Verify Email</a></p>`;
 
   mail.to = email;
-  // mail.html = `<h4>Hello, ${name}</h4>
-  // ${message}`;
   mail.html = `<!DOCTYPE html>
   <html lang="en">
     <head>
